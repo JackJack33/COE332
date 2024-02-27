64c4d48a2986d2396c36fb67749482161834b2ca
JackJack33 <jackjack33mc@gmail.com>
2024-02-27 09:33:43 -0600

    hw5 files

---
+++ homework05/README.md
@@ -0,0 +1,46 @@
+# International Space Station Tracker Flask App
+
+## Overview
+
+This project involves tracking the International Space Station by fetching data from NASA's website and analyzing various parameters (range, closest epoch, average speed, and instantaneous speed) in Python. This is also a Flask app, enabling data to be requested and sent over the internet.
+
+### Scripts
+`iss_tracker.py`
+This script contains functions for fetching, formatting, and analyzing the ISS data, alongside creating the Flask app. Functions include `fetch_data()`, `format_data()`, `calculate_data_range()`, `find_closest_epoch()`, `calculate_average_speed()`, `calculate_instantaneous_speed()`, `get_epochs()`, `get_epoch()`, `get_epoch_speed`, and `get_now()`.
+
+`test_iss_tracker.py`
+This is the pytest file for `iss_tracker.py`.
+
+### Running the Code
+#### Non-Containerized
+1. Make sure Python3 and necessary dependencies (`requests`, `xmltodict`, `math`, `logging`, `datetime`, `pytest`, `flask`) are installed on your computer.
+2. Download or clone this repository into a directory.
+3. Run the main script (`iss_tracker.py`) and/or the test script (`test_iss_tracker.py`). *(NOTE: You MUST run `iss_tracker.py` first to set up the flask app that is requested by `test_iss_tracker.py`)*
+4. See Flask Interaction below
+#### Containerized
+1. Make sure Docker is intalled on your computer
+2. Download or clone this repository into a directory.
+5. In the directory, run `docker build -t iss_tracker:1.0 .` And wait for the Docker image to generate.
+6. In the directory, run `docker run --name "flask-iss_tracker" -d -p 5000:5000 iss_tracker:1.0`
+7. To check things are up and running, run `docker ps -a`
+8. See Flask Interaction below
+
+### Flask Interaction
+You can interact with the flask app via `curl localhost:5000/<route>` once it is up and running.
+
+| Route                   | Description                                               |
+|-------------------------|-----------------------------------------------------------|
+| /epochs                 | Returns formatted epochs data                                    |
+| /epochs?limit=int&offset=int | Formatted epochs data starting at `offset` with `limit` entries  |
+| /epochs/<epoch>         | Returns state vector for a specific Epoch timestamp *(format: 'YYYY-MM-DD__HH_MM_SS.SSSSSS')*  |
+| /epochs/<epoch>/speed   | Returns instantaneous speed for a specific Epoch timestamp *(format: 'YYYY-MM-DD__HH_MM_SS.SSSSSS')* |
+| /now                    | Returns state vectors and instantaneous speed for the Epoch that is nearest in time |
+
+
+### Interpretation
+- The `fetch_data()` function fetches the ISS data from NASA's website.
+- The `format_data()` function parses the XML data into a list of dictionaries, each representing a state vector of the ISS at a specific timestamp.
+- The `calculate_data_range()` function calculates the range of data based on the first and last timestamps.
+- The `find_closest_epoch()` function finds the epoch closest to the current time.
+- The `calculate_average_speed()` function computes the average speed of the ISS using the formatted data.
+- The `calculate_instantaneous_speed()` function calculates the instantaneous speed of the ISS at the closest epoch.
---
+++ homework05/iss_tracker.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python3
+
+import requests
+import xmltodict
+import math
+import logging
+from typing import Dict, List, Any, Tuple
+from datetime import datetime
+from flask import Flask, request
+
+app = Flask(__name__)
+
+url = "https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml"
+
+def fetch_data() -> Dict[str, Any]:
+    """
+    Fetches data from the url variable and parses it as XML using xmltodict
+
+    Returns:
+        Dict[str, Any]: The parsed XML data as a dictionary
+    """
+    try:
+        response = requests.get(url)
+        if (response.status_code != 200):
+            raise requests.exceptions.RequestException
+        xml_data = response.text
+        return xmltodict.parse(xml_data)
+    except requests.exceptions.RequestException as exception:
+        logging.error(f"Error fetching data: {exception} {response}")
+        return
+    except xmltodict.expat.ExpatError as exception:
+        logging.error(f"Error parsing XML data: {exception} {response}")
+        return
+
+def format_data(data_dict: Dict[str, Any]) -> List[Dict[str, Any]]:
+    """
+    Parses/formats the XML dictionary into a list of dictionaries
+
+    Args:
+        data_dict (Dict[str, Any]): Dictionary containing the XML data
+
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries containing formatted data
+    """
+    formatted_data = []
+
+    try:
+        data_list = data_dict['ndm']['oem']['body']['segment']['data']['stateVector']
+    except KeyError:
+        logging.error("Error: Missing or incorrect key in data dictionary")
+        return formatted_data
+
+    for stateVector in data_list:
+        try:
+            timestamp = datetime.strptime(stateVector['EPOCH'], "%Y-%jT%H:%M:%S.%fZ")
+            timestamp = datetime.strftime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+            formatted_data.append({
+                'timestamp': timestamp,
+                'x': float(stateVector['X']['#text']),
+                'y': float(stateVector['Y']['#text']),
+                'z': float(stateVector['Z']['#text']),
+                'dx': float(stateVector['X_DOT']['#text']),
+                'dy': float(stateVector['Y_DOT']['#text']),
+                'dz': float(stateVector['Z_DOT']['#text']),
+            })
+        except KeyError:
+            logging.error("Error: Missing or incorrect key in state vector")
+            continue
+        except (ValueError, TypeError):
+            logging.error("Error: Unable to parse data to the correct format")
+            continue
+
+    return formatted_data
+
+def calculate_data_range(formatted_data: List[Dict[str, Any]]) -> Tuple[str, str]:
+    """
+    Calculates the range of data based on the first and last timestamps
+
+    Args:
+        formatted_data (List[Dict[str, Any]]): A list of dictionaries containing formatted data
+
+    Returns:
+        tuple[str]: The first and last timestamps
+    """
+    try:
+        first_epoch = formatted_data[0]['timestamp']
+        last_epoch = formatted_data[-1]['timestamp']
+        return first_epoch, last_epoch
+    except IndexError:
+        logging.error("Error: Empty formatted_data list")
+        return
+
+def find_closest_epoch(formatted_data: List[Dict[str, Any]]) -> Dict[str, Any]:
+    """
+    Finds the epoch closest to the current time in the formatted data
+
+    Args:
+        formatted_data (List[Dict[str, Any]]): A list of dictionaries containing formatted data
+
+    Returns:
+        Dict[str, [str,float]]: The dictionary representing the closest epoch
+    """
+    try:
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        current_time = datetime.strptime(current_time, '%Y-%m-%d %H:%M:%S.%f')
+        closest_epoch = min(formatted_data, key=lambda x: abs(current_time - datetime.strptime(x['timestamp'], '%Y-%m-%d %H:%M:%S.%f')))
+        return closest_epoch
+    except (ValueError, TypeError):
+        logging.error("Error: Unable to calculate closest epoch")
+        return
+
+def calculate_average_speed(formatted_data: List[Dict[str, Any]]) -> float:
+    """
+    Calculates the average speed based on the formatted data
+
+    Args:
+        formatted_data (List[Dict[str, Any]]): A list of dictionaries containing formatted data
+
+    Returns:
+        float: The average speed
+    """
+    try:
+        total_speed = 0
+        for data in formatted_data:
+            total_speed += math.sqrt(data['dx'] ** 2 + data['dy'] ** 2 + data['dz'] ** 2)
+        return total_speed / len(formatted_data)
+    except (KeyError, TypeError):
+        logging.error("Error: Missing or incorrect keys in closest_epoch")
+        return
+    except ZeroDivisionError:
+        logging.error("Error: Empty formatted_data list (division by zero)")
+        return
+
+def calculate_instantaneous_speed(closest_epoch: Dict[str, Any]) -> float:
+    """
+    Calculates the instantaneous speed based on the closest epoch
+
+    Args:
+        closest_epoch (Dict[str, Any]): The dictionary representing the closest epoch
+
+    Returns:
+        float: The instantaneous speed
+    """
+    try:
+        instantaneous_speed = math.sqrt(closest_epoch['dx'] ** 2 + closest_epoch['dy'] ** 2 + closest_epoch['dz'] ** 2)
+        return instantaneous_speed
+    except (KeyError, TypeError):
+        logging.error("Error: Missing or incorrect keys in closest_epoch")
+        return
+
+@app.route('/epochs', methods=['GET'])
+def get_epochs():
+    """
+    Fetches data and returns subset of epochs based on parameters
+
+    Returns:
+    - list: Formatted epochs data
+    """
+    data_dict = fetch_data()
+    formatted_data = format_data(data_dict)
+
+    limit = request.args.get('limit', default=None, type=int)
+    offset = request.args.get('offset', default=0, type=int)
+
+    if limit is not None:
+        formatted_data = formatted_data[offset:offset+limit]
+    else:
+        formatted_data = formatted_data[offset:]
+
+    return formatted_data
+
+@app.route('/epochs/<epoch>', methods=['GET'])
+def get_epoch(epoch):
+    """
+    Fetches data and returns specific epoch
+
+    Args:
+    - epoch (str): The timestamp for the epoch in format 'YYYY-MM-DD__HH_MM_SS.SSSSSS'
+
+    Returns:
+    - dict: A dictionary containing the epoch data if found
+    """
+    epoch = epoch.replace("__", " ").replace("_", ":")
+    data_dict = fetch_data()
+    formatted_data = format_data(data_dict)
+    for data in formatted_data:
+        if data['timestamp'] == epoch:
+            return data
+    return {'error': 'Epoch not found'}, 404
+
+@app.route('/epochs/<epoch>/speed', methods=['GET'])
+def get_epoch_speed(epoch):
+    """
+    Fetches data and returns a specific epoch's instantaneous speed
+
+    Args:
+    - epoch (str): The timestamp for the epoch in format 'YYYY-MM-DD__HH_MM_SS.SSSSSS'
+
+    Returns:
+    - dict: A dictionary containing the epoch's state vector and instantaneous speed
+    """
+    epoch = epoch.replace("__", " ").replace("_", ":")
+    data_dict = fetch_data()
+    formatted_data = format_data(data_dict)
+    for data in formatted_data:
+        if data['timestamp'] == epoch:
+            speed = calculate_instantaneous_speed(data)
+            return {'speed': speed}
+    return {'error': 'Epoch not found'}, 404
+
+@app.route('/now', methods=['GET'])
+def get_now():
+    """
+    Retrieves data for the closest epoch to the current time and calculates its speed
+
+    Returns:
+    - dict: A dictionary containing the closest epoch timestamp and its associated speed
+    """
+    data_dict = fetch_data()
+    formatted_data = format_data(data_dict)
+    closest_epoch = find_closest_epoch(formatted_data)
+    speed = calculate_instantaneous_speed(closest_epoch)
+    return {'closest_epoch': closest_epoch, 'speed': speed}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
