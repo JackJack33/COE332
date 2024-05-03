ae63860fc5a8880ec28da669ff5bd7e52954c899
JackJack33 <jackjack33mc@gmail.com>
2024-05-03 17:18:22 -0500

    worker attempt

--- homework08/src/test_worker.py
+++ homework08/src/test_worker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 import pytest
 from worker import get_same_group_ids
 from api import get_gene_data
@@ -9,10 +10,10 @@ from api import get_gene_data
 def test_get_same_group_ids():
     hgnc_id = 'HGNC:11501'
     count = 5
-    group = get_gene_data(hgnc_id)['gene_group']
+    group = jsonify(get_gene_data(hgnc_id)).get('gene_group')
 
     selected_ids = get_same_group_ids(hgnc_id, count)
     assert len(selected_ids) <= count
 
     for gene_id in selected_ids:
-        assert get_gene_data(gene_id).get['gene_group'] == group
+        assert jsonify(get_gene_data(gene_id)).get('gene_group') == group
--- homework08/src/worker.py
+++ homework08/src/worker.py
@@ -1,15 +1,16 @@
+import json
 from jobs import get_job_by_id, update_job_status, q, rd
 from api import get_gene_data, get_gene_ids
 import time
 
 def get_same_group_ids(hgnc_id, count):
-    hgnc_data = get_gene_data(hgnc_id)
-    group = hgnc_data['gene_group']
+    hgnc_data = jsonify(get_gene_data(hgnc_id))
+    group = hgnc_data.get('gene_group')
 
     gene_ids = get_gene_ids()
     same_group_ids = [gene_id for gene_id in gene_ids
                       if (gene_id != hgnc_id) and
-                      get_gene_data(gene_id)['gene_group'] == group]
+                      jsonify(get_gene_data(gene_id)).get('gene_group') == group]
     selected_ids = same_group_ids[:count]
 
     return selected_ids
