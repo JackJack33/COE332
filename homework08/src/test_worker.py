f3e31c186e5054cb6869129a4e24313381e36c09
JackJack33 <jackjack33mc@gmail.com>
2024-05-03 16:36:19 -0500

    worker fix 2

--- homework08/src/test_worker.py
+++ homework08/src/test_worker.py
@@ -9,10 +9,10 @@ from api import get_gene_data
 def test_get_same_group_ids():
     hgnc_id = 'HGNC:11501'
     count = 5
-    group = get_gene_data(hgnc_id)['group']
+    group = get_gene_data(hgnc_id)['gene_group']
 
     selected_ids = get_same_group_ids(hgnc_id, count)
     assert len(selected_ids) <= count
 
     for gene_id in selected_ids:
-        assert get_gene_data(gene_id).get['group'] == group
+        assert get_gene_data(gene_id).get['gene_group'] == group
